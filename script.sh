e49f13f24d10e069d7e0169737d44b2b30fcb395
Sujetha Pai Krishna Pai <122850668+suthapai@users.noreply.github.com>
2025-02-21 18:06:01 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,4 @@
-#!/bin/bash
-#installing docker
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo usermod -aG docker shubham
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo chmod 666 /var/run/docker.sock
-#installing Kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
 
-#installing AzureCLI
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-sudo apt-get update
 
 
 
